72bfb8f973809bf183e962f273543417488ce133
123xxa <lyy997807@gmail.com>
2023-12-21 17:46:01 +0800

    导航栏图标问题

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,15 @@ const ComponentsPlugin = require('unplugin-vue-components/webpack');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave:false,
+  pwa:{
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
+  },
   configureWebpack:{
     plugins: [
       ComponentsPlugin.default({ resolvers: [VantResolver()] }), //当 unplugin-vue-components 版本大于等于 0.26.0
